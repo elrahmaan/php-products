822360fbf00aaf23c9258233d28f1df2297173f9
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-12-24 02:18:39 +0700

    connection

--- connection.php
+++ connection.php
@@ -1,14 +1,14 @@
 <?php
 //for MySQLi OOP
-$conn = new mysqli('10.0.0.74', 'admin', 'Uchiaatew17*', 'storedb');
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
-}
-	////////////////
+// $conn = new mysqli('10.0.0.74', 'admin', 'Uchiaatew17*', 'storedb');
+// if ($conn->connect_error) {
+// 	die("Connection failed: " . $conn->connect_error);
+// }
+////////////////
 
-	//for MySQLi Procedural
-	// $conn = mysqli_connect('localhost', 'root', '', 'mydatabase');
-	// if(!$conn){
-	//     die("Connection failed: " . mysqli_connect_error());
-	// }
-	////////////////
+// for MySQLi Procedural
+$conn = mysqli_connect('10.0.0.74', 'admin', 'Uchiaatew17*', 'storedb');
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+	//////////////
